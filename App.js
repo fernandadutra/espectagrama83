3d5145ac54606a59d82cf2b3822840aebe23678e
fernandadutra <100439986+fernandadutra@users.noreply.github.com>
2023-07-19 16:44:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import DrawerNavigator from "./navigation/DrawerNavigation";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator></DrawerNavigator>
+    </NavigationContainer>
+  );
+}
